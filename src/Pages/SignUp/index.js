52a420da280a915eec0a9b73a036c2b372ab9c5d
Paul Rinaldi <paulrinaldi10@yahoo.com>
2019-11-02 13:36:51 -0500

    Adds writablility to Focus TextFields

--- src/Pages/SignUp/index.js
+++ src/Pages/SignUp/index.js
@@ -15,16 +15,24 @@ class SignUp extends React.Component {
             ...INITIAL_STATE
         };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeF1 = this.handleChangeF1.bind(this);
+        this.handleChangeF2 = this.handleChangeF2.bind(this);
+        this.handleChangeF3 = this.handleChangeF3.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     };
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
+    handleChangeF1(event) {
+        this.setState({f1: event.target.value});
+    };
+    handleChangeF2(event) {
+        this.setState({f2: event.target.value});
+    };
+    handleChangeF3(event) {
+        this.setState({f3: event.target.value});
     };
     
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        alert('A name was submitted: ' + this.state.f1 + this.state.f2 + this.state.f3);
         event.preventDefault();
     };
 
@@ -35,9 +43,9 @@ class SignUp extends React.Component {
                     Choose your focuses 
                 </Typography>
                 <form onSubmit={this.handleSubmit}>
-                    <TextField margin="normal" label="Skill 1" value={this.state.f1} onChange={this.handleChange} type="text" placeholder="Ex. Natural Language Processing" required />
-                    <TextField margin="normal" label="Skill 2" value={this.state.f2} onChange={this.handleChange} type="text" placeholder="Ex. Leadership" required />
-                    <TextField margin="normal" label="Skill 3" value={this.state.f3} onChange={this.handleChange} type="text" placeholder="Ex. React JS" required />
+                    <TextField margin="normal" label="Skill 1" value={this.state.f1} onChange={this.handleChangeF1} type="text" placeholder="Ex. Natural Language Processing" required />
+                    <TextField margin="normal" label="Skill 2" value={this.state.f2} onChange={this.handleChangeF2} type="text" placeholder="Ex. Leadership" required />
+                    <TextField margin="normal" label="Skill 3" value={this.state.f3} onChange={this.handleChangeF3} type="text" placeholder="Ex. React JS" required />
                     <Button block value="Submit" type="submit" variant='contained'>
                         <Typography>
                             Continue
